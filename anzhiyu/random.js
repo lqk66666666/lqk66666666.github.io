995fe6a58dcaf168c29cae6df7c6569627d648fd
lqk <3216552099@qq.com>
2024-04-07 16:40:04 +0800

    Site updated: 2024-04-07 16:40:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/2eec1551.html","posts/7c63bf7d.html"];function toRandomPost(){
+var posts=["posts/2eec1551.html","posts/4a17b156.html","posts/7c63bf7d.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
